333b67770ef3124f403f97cec44a9e80cad92fbf
Pascal Charpentier <charpentier_pasc@hotmail.com>
2025-04-29 21:20:48 -0400

    BooleanExpression.cs

--- BooleanExpression/BooleanExpression.cs
+++ BooleanExpression/BooleanExpression.cs
@@ -18,13 +18,67 @@ public class BooleanExpression
         return _nodes[nodeId] ;
     }
 
-    public void ExpandNode(Guid nodeId, Operator operatorValue)
+    private void AuxCollapseNode(BasicNode node)
     {
-        var node = GetNodeFromId(nodeId) ;
-        if (node.HasChildren()) throw new Exception( "Node cannot be expanded." ) ;
-        
-        
+        if (!node.HasChildren()) return ;
+
+        var nt = node as NonTerminalNode ?? throw new Exception("A childless node has children") ;
+        foreach (var child in nt.Children)
+        {
+            AuxCollapseNode(child) ;
+            _nodes.Remove(child.GetNodeId()) ;
+        }
     }
-    
-    
+
+    public void CollapseNode(Guid nodeId)
+    {
+        var node = GetNodeFromId(nodeId) as NonTerminalNode ?? throw new Exception("Node is not non-terminal") ;
+        if (!node.HasChildren()) throw new Exception("Node can not be collapsed") ;
+        AuxCollapseNode(node) ;
+    }
+
+    public void AddVariableProduction(Guid nodeId, int value)
+    {
+        var node = GetNodeFromId(nodeId) as NonTerminalNode ?? throw new Exception("Node is not a non-terminal node") ;
+        if (node.HasChildren()) throw new Exception("Node cannot be expanded.") ;
+
+        var newNode = new IdNode(value) ;
+
+        node.AddChildren([newNode]) ;
+        _nodes.Add(newNode.GetNodeId(), newNode) ;
+    }
+
+    public void AddBinaryOperatorProduction(Guid nodeId, Operator op)
+    {
+        var node = GetNodeFromId(nodeId) as NonTerminalNode ?? throw new Exception("Node is not a non-terminal node") ;
+        if (node.HasChildren()) throw new Exception("Node cannot be expanded.") ;
+
+        var lhsNode = new ENode() ;
+        var rhsNode = new ENode() ;
+
+        node.AddChildren([lhsNode, new OpNode(op), rhsNode]) ;
+        _nodes.Add(lhsNode.GetNodeId(), lhsNode) ;
+        _nodes.Add(rhsNode.GetNodeId(), rhsNode) ;
+    }
+
+    public void AddUnaryOperatorProduction(Guid nodeId, Operator op)
+    {
+        var node = GetNodeFromId(nodeId) as NonTerminalNode ?? throw new Exception("Node is not a non-terminal node") ;
+        if (node.HasChildren()) throw new Exception("Node cannot be expanded.") ;
+        var lhsNode = new ENode() ;
+        
+        node.AddChildren([new OpNode(op), lhsNode]) ;
+        _nodes.Add(lhsNode.GetNodeId(), lhsNode) ;
+    }
+
+    public void AddParenthesisProduction(Guid nodeId)
+    {
+        var node = GetNodeFromId(nodeId) as NonTerminalNode ?? throw new Exception("Node is not a non-terminal node") ;
+        if (node.HasChildren()) throw new Exception("Node cannot be expanded.") ;
+
+        var newNode = new ENode() ;
+
+        node.AddChildren([new OpNode(Operator.Leftparen), newNode, new OpNode(Operator.Rightparen)]) ;
+        _nodes.Add(newNode.GetNodeId(), newNode) ;
+    }
 }
--- BooleanExpression/Node/NonTerminalNode.cs
+++ BooleanExpression/Node/NonTerminalNode.cs
@@ -2,7 +2,7 @@ namespace BooleanExpression.Node ;
 
 public abstract class NonTerminalNode : BasicNode
 {
-    private  List<BasicNode> Children { get ;  } = new() ;
+    public  List<BasicNode> Children { get ;  } = new() ;
 
     public void AddChildren(List<BasicNode> newChildren)
     {
